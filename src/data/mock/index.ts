a473e1bae5ed1b8d96f8568ef0d1e37ff9d1e842
Vadim Sultanov <vadim.sultanov@quadcode.com>
2026-09-09 13:40:51 +0300

    feat: three mock exam variants in official KEG A1 reading format

---
+++ src/data/mock/index.ts
@@ -0,0 +1,6 @@
+import type { MockVariant } from '../../types/mock'
+import { VARIANT_1 } from './variant1'
+import { VARIANT_2 } from './variant2'
+import { VARIANT_3 } from './variant3'
+
+export const VARIANTS: MockVariant[] = [VARIANT_1, VARIANT_2, VARIANT_3]
